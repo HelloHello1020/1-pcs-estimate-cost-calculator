08bcb1292a02b26b050cc16f64fdac549e9052e7
HelloHello1020 <hellologisticsthailand@example.com>
2025-02-15 21:19:15 +0700

    project v1

--- src/App.js
+++ src/App.js
@@ -46,9 +46,6 @@ function App() {
       else if (weight >= 15.1 && weight <= 20) {
         if (length > 24 || width > 24 || height > 24) {
           estimateCost.current.innerText = `฿${Math.round(weight * 25)}\n(click the link below to negotiate about the extra box size)`
-          pageLink.current.href = "https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr"
-          pageLink.current.target = "_blank"
-          pageLink.current.innerText = "Hello Logistics Thailand Page"
         }
         else {
           estimateCost.current.innerText = `฿${Math.round(weight * 25)}`
@@ -57,16 +54,10 @@ function App() {
 
       else if (weight >= 20.1 && weight <= 100) {
         estimateCost.current.innerText = `฿${Math.round(weight * 20)}\n(click the link below to negotiate about the full price)`
-        pageLink.current.href = "https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr"
-        pageLink.current.target = "_blank"
-        pageLink.current.innerText = "Hello Logistics Thailand Page"
       }
 
       else {
         estimateCost.current.innerText = `Too heavy for estimation\n(click the link below to negotiate about the price)`
-        pageLink.current.href = "https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr"
-        pageLink.current.target = "_blank"
-        pageLink.current.innerText = "Hello Logistics Thailand Page"
       }
     }
 
@@ -108,17 +99,114 @@ function App() {
       }
 
       else if (weight >= 10.1 && weight <= 100) {
-        estimateCost.current.innerText = `฿${Math.round(weight * 45)}\n(click the link below to negotiate about the full price)`
-        pageLink.current.href = "https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr"
-        pageLink.current.target = "_blank"
-        pageLink.current.innerText = "Hello Logistics Thailand Page"
+        estimateCost.current.innerText = `฿${Math.round(weight * 45)}\nClick the link below to negotiate about the full price`
       }
 
       else {
-        estimateCost.current.innerText = `฿${Math.round(weight * 35)}\n(click the link below to negotiate about the full price)`
-        pageLink.current.href = "https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr"
-        pageLink.current.target = "_blank"
-        pageLink.current.innerText = "Hello Logistics Thailand Page"
+        estimateCost.current.innerText = `฿${Math.round(weight * 35)}\n(Only using weight)\nClick the link below to negotiate about the full price`
+      }
+    }
+
+    else if ((selectedOption1 === "Maesot" && selectedOption2 === "Myawady") || (selectedOption1 === "Myawady"  && selectedOption2 === "Maesot")) {
+      if (weight <= 5) {
+        if (length > 12 || width > 12 || height > 12) {
+          estimateCost.current.innerText = `฿180 (฿30 for extra box size)`
+        }
+        else {
+          estimateCost.current.innerText = `฿150`
+        }
+      }
+
+      else if (weight >= 5.1 && weight <= 20) {
+        if (length > 24 || width > 24 || height > 24) {
+          estimateCost.current.innerText = `฿${Math.round(weight * 35 + 20)} (฿20 for extra box size)`
+        }
+        else {
+          estimateCost.current.innerText = `฿${Math.round(weight * 35)}`
+        }
+      }
+
+      else {
+        estimateCost.current.innerText = `฿${Math.round(weight * 30)}\n(Only using weight)\nClick the link below to negotiate about the full price`
+      }
+    }
+
+    else if ((selectedOption1 === "Chiang Mai" && selectedOption2 === "Yangon") || (selectedOption1 === "Chiang Mai" && selectedOption2 === "Mandalay") || (selectedOption1 === "Yangon" && selectedOption2 === "Chiang Mai") || (selectedOption1 === "Mandalay" && selectedOption2 === "Chiang Mai")) {
+      if (weight < 2) {
+        if (length > 6 || width > 6 || height > 6) {
+          estimateCost.current.innerText = `฿170 (฿20 for extra box size)`
+        }
+        else {
+          estimateCost.current.innerText = `฿150`
+        }
+      }
+
+      else if (weight >= 2.1 && weight <= 3) {
+        if (length > 8 || width > 8 || height > 8) {
+          estimateCost.current.innerText = `฿230 (฿30 for extra box size)`
+        }
+        else {
+          estimateCost.current.innerText = `฿200`
+        }
+      }
+
+      else if (weight >= 3.1 && weight <= 4) {
+        if (length > 12 || width > 12 || height > 12) {
+          estimateCost.current.innerText = `฿350 (฿50 for extra box size)`
+        }
+        else {
+          estimateCost.current.innerText = `฿300`
+        }
+      }
+
+      else if (weight >= 4.1 && weight <= 10) {
+        if (length > 18 || width > 18 || height > 18) {
+          estimateCost.current.innerText = `฿${Math.round(weight * 60)}\n(Only using weight)\nClick the link below to negotiatw about the full price`
+        }
+        else {
+          estimateCost.current.innerText = `฿${Math.round(weight * 60)}`
+        }
+      }
+
+      else if (weight >= 10.1 && weight <= 100) {
+        estimateCost.current.innerText = `฿${Math.round(weight * 55)}\n(Only using weight)\nClick the link below to negotiate about the full price`
+      }
+
+      else {
+        estimateCost.current.innerText = `฿${Math.round(weight * 45)}\n(Only using weight)\nClick the link below to negotiate about the full price`
+      }
+    }
+
+    else if ((selectedOption1 === "Bangkok" && (selectedOption2 === "Mandalay" || selectedOption2 === "Yangon"))) {
+      if (weight <= 4) {
+        if (length > 10 || width > 10 || height > 10) {
+          estimateCost.current.innerText = `฿350\n(Only using weight)\nClick the link below to negotiate about the full price`
+        }
+        else {
+          estimateCost.current.innerText = `฿350`
+        }
+      }
+
+      else if (weight >= 4.1 && weight <= 10) {
+        if (length > 14 || width > 14 || height > 14) {
+          estimateCost.current.innerText = `฿${weight * 65}\n(Only using weight)\nClick the link below to negotiate about the full price`
+        }
+        else {
+          estimateCost.current.innerText = `฿${weight * 65}`
+        }
+      }
+
+      else if (weight >= 10.1 && weight <= 100) {
+        if (length > 18 || width > 18 || height > 18) {
+          estimateCost.current.innerText = `฿${weight * 55}\n(Only using weight)\nClick the link below to negotiate about the full price`
+        }
+        else {
+          estimateCost.current.innerText = `฿${weight * 55}`
+        }
+      }
+
+      else {
+        estimateCost.current.innerText = `฿${weight * 50}\n(Only using weight)\nClick the link below to negotiate about the full price`
       }
     }
   };
@@ -198,7 +286,7 @@ function App() {
       </div>
 
       <h1 className="estimate-cost" ref={estimateCost}>Estimated Cost</h1>
-      <a ref={pageLink}></a>
+      <a className="page-link" ref={pageLink} href="https://www.facebook.com/share/1Dsn9K7jrQ/?mibextid=wwXIfr" target="_blank">Hello Logistics Thailand Page</a>
     </div>
   );
 }
