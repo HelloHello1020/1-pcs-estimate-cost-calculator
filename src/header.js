1b54d862eb3820f73ae69d43e492656aa9ce2c98
HelloHello1020 <hellologisticsthailand@gmail.com>
2025-03-05 00:07:28 +0700

    website v22 (thingyan)

---
+++ src/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img
+          src="/hello_logistics_thailand_logo.png"
+          alt="company-logo"
+          className="company-logo"
+        />
+
+        <h1 className="title">Estimate Cost Calculator</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
